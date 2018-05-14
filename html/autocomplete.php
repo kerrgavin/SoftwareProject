c5a5e34189a04f9aa8fab714a6d1200546447c7f
kerrgavin <kerr_gavin@columbusstate.edu>
2018-05-14 18:19:43 -0400

    5/14/18

--- html/autocomplete.php
+++ html/autocomplete.php
@@ -1,28 +1,14 @@
 <?php
 ini_set('display_errors', 1);
 
-
-
-if (isset($_GET['name']) AND isset($_GET['country'])) {
-        if(($sname = $_GET['name']) && ($scountry = $_GET['country'])) {
-                $url = "http://autocomplete.wunderground.com/aq?query=$sname&c=$scountry";
-                //echo $url;
-                echo "";
-        $response = file_get_contents($url);
-        //echo $response;
-        $d = json_decode($response);
-        echo json_encode($d);
-        }
-}
-
 //This block takes checks to see if the 'q' is used
 //and then parses the appropriate api call using the
 //'id' information and the api key
-else if (isset($_GET['name'])) {
+if (isset($_GET['name'])) {
 	if(($sname = $_GET['name'])) {
-		$url = "http://autocomplete.wunderground.com/aq?query=$sname";
+		$n = str_replace(" ","%20",$sname);
+		$url = "http://autocomplete.wunderground.com/aq?query=$n";
 		//echo $url;
-		echo "";
         $response = file_get_contents($url);
 	//echo $response;
         $d = json_decode($response);
--- html/search.js
+++ html/search.js
@@ -1,71 +1,127 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-   var getInfo = function(){
+  var getInfo = function() {
 
-        var loc = $('#loc').val();
-		var state = $('#state').val();
+    var loc = $('#loc').val();
+    var locs = loc.split(",");
+    for (var i = 0; i < locs.length; i++) {
+      var temp = locs[i].trim();
+      console.log("Variable temp: " + temp);
+      var s = "";
+      for (var j = 0; j < temp.length; j++) {
+        if (temp.charAt(j) != ' ') {
+          s += temp.charAt(j);
+        } else {
+          s += "_";
+        }
+      }
+      locs[i] = s;
+      console.log("Variable s: " + s);
+    }
 
-         if(loc == ''){
+    console.log(locs[0] + " " + locs[1]);
 
-            $('#info').html("<h2 class='loading'>Enter something.</h2>");
+    if (loc == '') {
 
-         } else {
+      $('#info').html("<h2 class='loading'>Enter something.</h2>");
 
-            $('#info').html("<img src='giphy.webp'>");
+    } else {
 
-			if(document.getElementById("currentCond").checked) {
-				$.getJSON("currentConditions.php?name=" + loc + "&state="+state, function(json) {
-					$('#info').html('<section id="i"><h2 class="loading">' + json.current_observation.display_location.full + '</h2><p><img id="icon" src='+ json.current_observation.icon_url+' alt='+json.current_observation.icon+' >' + json.current_observation.weather + '<br>Temperature: ' + json.current_observation.temperature_string +'<br>Relative Humidity: ' + json.current_observation.relative_humidity + '<br>Wind Speed: ' + json.current_observation.wind_mph + '<br>Feels Like: ' + json.current_observation.feelslike_string + '</p></section>');
-				});
-			} else if(document.getElementById("almanac").checked) {
-				$.getJSON("almanac.php?name=" + loc + "&state="+state, function(json) {
-					$('#info').html('<section id="i"><h2 class="loading">Almanac</h2><p><h3>High</h3>Normal: ' + json.almanac.temp_high.normal.F + ' F (' + json.almanac.temp_high.normal.C + ' C)<br>Record: ' +  json.almanac.temp_high.record.F +' F (' +  json.almanac.temp_high.record.C + ' C) ' + json.almanac.temp_high.recordyear + '<br><h3>Low</h3>Normal: ' + json.almanac.temp_low.normal.F + ' F (' +  json.almanac.temp_low.normal.C + ' C) <br>Record: ' + json.almanac.temp_low.record.F + ' F (' +  json.almanac.temp_low.record.C + ' C) ' + json.almanac.temp_low.recordyear + '</p></section>');
-				});
-			} else if(document.getElementById("3Day").checked) {
-				$.getJSON("threeDayForecast.php?name=" + loc + "&state="+state, function(json) {
+      $('#info').html("<img src='giphy.webp'>");
+
+      if (document.getElementById("currentCond").checked) {
+        console.log("currentConditions.php?name=" + locs[0] + "&state=" + locs[1]);
+        $.getJSON("currentConditions.php?name=" + locs[0] + "&state=" + locs[1], function(json) {
+          if (json.hasOwnProperty('current_observation')) {
+            $('#info').html('<section id="i"><h2 class="loading">' + json.current_observation.display_location.full + '</h2><p><img id="icon" src=' + json.current_observation.icon_url + ' alt=' + json.current_observation.icon + ' >' + json.current_observation.weather + '<br>Temperature: ' + json.current_observation.temperature_string + '<br>Relative Humidity: ' + json.current_observation.relative_humidity + '<br>Wind Speed: ' + json.current_observation.wind_mph + '<br>Feels Like: ' + json.current_observation.feelslike_string + '</p></section>');
+          } else {
+            $('#info').html('<section id="i"><h3>Please Enter a valid location!</h3></section>');
+          }
+        });
+      } else if (document.getElementById("almanac").checked) {
+        $.getJSON("almanac.php?name=" + locs[0] + "&state=" + locs[1], function(json) {
+          if(json.hasOwnProperty('almanac')){
+          $('#info').html('<section id="i"><h2 class="loading">Almanac</h2><p><h3>High</h3>Normal: ' + json.almanac.temp_high.normal.F + ' F (' + json.almanac.temp_high.normal.C + ' C)<br>Record: ' + json.almanac.temp_high.record.F + ' F (' + json.almanac.temp_high.record.C + ' C) ' + json.almanac.temp_high.recordyear + '<br><h3>Low</h3>Normal: ' + json.almanac.temp_low.normal.F + ' F (' + json.almanac.temp_low.normal.C + ' C) <br>Record: ' + json.almanac.temp_low.record.F + ' F (' + json.almanac.temp_low.record.C + ' C) ' + json.almanac.temp_low.recordyear + '</p></section>');
+        } else {
+          $('#info').html('<section id="i"><h3>Please Enter a valid location!</h3></section>');
+        }
+        });
+      } else if (document.getElementById("3Day").checked) {
+
+        $.getJSON("threeDayForecast.php?name=" + locs[0] + "&state=" + locs[1], function(json) {
+          if(json.hasOwnProperty('forecast')){
           var f = json.forecast.simpleforecast.forecastday;
           var s = '<section id="i">';
           for (var i = 0; i < f.length; i++) {
-            s+='<p>Date: ' + f[i].date.pretty + '<br>High: ' + f[i].high.fahrenheit + ' F (' + f[i].high.celsius + ' C)<br>Low: ' + f[i].low.fahrenheit + ' F (' + f[i].low.celsius + ' C)<br>Condition: ' + f[i].conditions + '<img id="icon" src='+ f[i].icon_url+' alt='+f[i].icon+' ><br>Max Wind Speed:' + f[i].maxwind.mph + ' mph (' + f[i].maxwind.kph + ' kph) ' + f[i].maxwind.dir + ' ' + f[i].maxwind.degrees + '<br>Average Wind Speed:' + f[i].avewind.mph + ' mph (' + f[i].avewind.kph + ' kph) ' + f[i].avewind.dir + ' ' + f[i].avewind.degrees + '<br>Humidity: ' + f[i].avehumidity + '(avg) ' + f[i].maxhumidity + '(max) ' + f[i].minhumidity + '(min)<br>Rainfall: ' + f[i].qpf_allday.in + 'in (' + f[i].qpf_allday.mm + 'mm)<br>Snowfall: ' + f[i].snow_allday.in + 'in (' + f[i].snow_allday.cm + 'cm)</p>';
-            s+='<br>';
+            s += '<p>Date: ' + f[i].date.pretty + '<br>High: ' + f[i].high.fahrenheit + ' F (' + f[i].high.celsius + ' C)<br>Low: ' + f[i].low.fahrenheit + ' F (' + f[i].low.celsius + ' C)<br>Condition: ' + f[i].conditions + '<img id="icon" src=' + f[i].icon_url + ' alt=' + f[i].icon + ' ><br>Max Wind Speed:' + f[i].maxwind.mph + ' mph (' + f[i].maxwind.kph + ' kph) ' + f[i].maxwind.dir + ' ' + f[i].maxwind.degrees + '<br>Average Wind Speed:' + f[i].avewind.mph + ' mph (' + f[i].avewind.kph + ' kph) ' + f[i].avewind.dir + ' ' + f[i].avewind.degrees + '<br>Humidity: ' + f[i].avehumidity + '(avg) ' + f[i].maxhumidity + '(max) ' + f[i].minhumidity + '(min)<br>Rainfall: ' + f[i].qpf_allday.in + 'in (' + f[i].qpf_allday.mm + 'mm)<br>Snowfall: ' + f[i].snow_allday.in + 'in (' + f[i].snow_allday.cm + 'cm)</p>';
+            s += '<br>';
           }
           s += '</secion>';
-					$('#info').html(s);
-				});
-			} else if(document.getElementById("astronomy").checked) {
-				$.getJSON("astronomy.php?name=" + loc + "&state="+state, function(json) {
-					$('#info').html('<section id="i"><h2 class="loading">Astronomy</h2><p>Moon Illumination: ' + json.moon_phase.percentIlluminated + '<br>Phase Time: ' +  json.moon_phase.current_time.hour + ":" + json.moon_phase.current_time.minute +'<br>Sunrise: ' + json.moon_phase.sunrise.hour + ":" + json.moon_phase.sunrise.minute +'<br>Sunset: ' + json.moon_phase.sunset.hour +  ":" + json.moon_phase.sunset.minute + '</p></section>');
-				});
-			} else if(document.getElementById("geo").checked) {
-				$.getJSON("geoLookUp.php?name=" + loc + "&state="+state, function(json) {
+          $('#info').html(s);
+        } else {
+          $('#info').html('<section id="i"><h3>Please Enter a valid location!</h3></section>');
+        }
+        });
+      } else if (document.getElementById("astronomy").checked){
 
+        $.getJSON("astronomy.php?name=" + locs[0] + "&state=" + locs[1], function(json) {
+          if(json.hasOwnProperty('moon_phase')){
+          $('#info').html('<section id="i"><h2 class="loading">Astronomy</h2><p>Moon Illumination: ' + json.moon_phase.percentIlluminated + '<br>Phase Time: ' + json.moon_phase.current_time.hour + ":" + json.moon_phase.current_time.minute + '<br>Sunrise: ' + json.moon_phase.sunrise.hour + ":" + json.moon_phase.sunrise.minute + '<br>Sunset: ' + json.moon_phase.sunset.hour + ":" + json.moon_phase.sunset.minute + '</p></section>');
+        } else {
+          $('#info').html('<section id="i"><h3>Please Enter a valid location!</h3></section>');
+        }
+        });
+      } else if (document.getElementById("geo").checked) {
+        console.log("geoLookUp.php?name=" + locs[0] + "&state=" + locs[1]);
+        $.getJSON("geoLookUp.php?name=" + locs[0] + "&state=" + locs[1], function(json) {
+          if(json.hasOwnProperty('location')){
           var f = json.location.nearby_weather_stations.airport;
           var s = '<section id="i"><h2 class="loading">Geolookup</h2>';
           s += '<h3>Airports</h3>';
           for (var i = 0; i < f.station.length; i++) {
-            s+='<p>Airport Code: ' + f.station[i].icao + '<br>Location: ' + f.station[i].city + ', ' + f.station[i].state +', ' + f.station[i].country +'</p>';
+            s += '<p>Airport Code: ' + f.station[i].icao + '<br>Location: ' + f.station[i].city + ', ' + f.station[i].state + ', ' + f.station[i].country + '</p>';
           }
           f = json.location.nearby_weather_stations.pws;
           s += '<h3>Weather Stations</h3>';
           for (var i = 0; i < f.station.length; i++) {
-            s+='<p>Station Code: ' + f.station[i].id + '<br>Location: ' + f.station[i].city + ', ' + f.station[i].state +', ' + f.station[i].country +'</p>';
+            s += '<p>Station Code: ' + f.station[i].id + '<br>Location: ' + f.station[i].city + ', ' + f.station[i].state + ', ' + f.station[i].country + '</p>';
           }
           s += '</secion>';
 
-					$('#info').html(s);
-				});
-			}
+          $('#info').html(s);
+        } else {
+            $('#info').html('<section id="i"><h3>Please Enter a valid location!</h3></section>');
+        }
+        });
+      }
+    }
 
-          }
+    return false;
+  }
 
-        return false;
-   }
+  var getPlace = function() {
+    var options = $('#places');
+    var loc = $('#loc').val();
 
-   $('#sub').click(getInfo);
-   $('#loc').keyup(function(event){
-       if(event.keyCode == 13){
-           getInfo();
-       }
-   });
+
+    options.empty();
+    var url = "autocomplete.php?name=" + loc;
+    console.log(url);
+    // Populate dropdown with list of provinces
+    $.getJSON(url, function(data) {
+      $.each(data.RESULTS, function(key, entry) {
+        options.append($('<option>').attr('value', entry.name));
+      })
+    });
+    return false;
+  }
+
+  $('#sub').click(getInfo);
+  $('#loc').keyup(function(event) {
+    if (event.keyCode == 13) {
+      getInfo();
+    }
+    getPlace();
+  });
 
 });
